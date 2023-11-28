641b4320b7a62a83e6c577c36daea37a05be810d
Mac Van Thanh <macthanhkmhd2003@gmail.com>
2023-11-28 10:03:35 +0700

    Update 28-11

--- Python/PY04014.py
+++ Python/PY04014.py
@@ -1,33 +1,33 @@
+class Student:
+    def __init__(self, idx, Name, p):
+        self.ID = "HS" + "{:02d}".format(idx)
+        self.Name = Name
+        self.diemTB = p[0] * 2.0 + p[1] * 2.0
+        for i in range(2, 10):
+            self.diemTB += p[i]
+        self.diemTB = self.diemTB / 10 / 1.2
+        if self.diemTB >= 9.0:
+            self.xepLoai = "XUAT SAC"
+        elif self.diemTB >= 8.0:
+            self.xepLoai = "GIOI"
+        elif self.diemTB >= 7.0:
+            self.xepLoai = "KHA"
+        elif self.diemTB >= 5.0:
+            self.xepLoai = "TB"
+        else:
+            self.xepLoai = "YEU"
+    def getInfo(self):
+        return "{} {} {:.1f} {}".format(self.ID, self.Name, round(self.diemTB, 2), self.xepLoai)
 
+def cmp(a):
+    return (a.diemTB, (-1) * a.ID)
 
-import math
+List = []
 
-class Score:
-    def __init__(self, id, name, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10) :
-        self.__id = id
-        self.__name = name
-        self.__d1 = d1
-        self.__d2 = d2
-        self.__d3 = d3
-        self.__d4 = d4
-        self.__d5 = d5
-        self.__d6 = d6
-        self.__d7 = d7
-        self.__d8 = d8
-        self.__d9 = d9
-        self.__d10 = d10
-    
-        
+for i in range(int(input())):
+    List.append(Student(i + 1, input(), list(map(float, input().split()))))
 
-if __name__ == '__main__':
-    
-    n = int(input())
-    
-    arr = []
-    for i in range(n) :
-        name = input()
-        Scores = [float(x) for x in input().split()]
-        x = Score(i + 1, name, Scores[0], Scores[1], Scores[2], Scores[3], Scores[4], Scores[5], Scores[6], Scores[7], Scores[8], Scores[9], Scores[10])
-    
-    for Score in arr:
-        print(Score)    
+List.sort(key=cmp, reverse=True)
+
+for i in List:
+    print(i.getInfo())
